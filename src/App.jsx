a596d77c3bf129bc4016d265124c9e7135323cb4
maarten <van_den_bergmaarten@hotmail.com>
2023-11-22 14:13:24 +0100

    added editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import { Editor } from '@monaco-editor/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <>        
-    </>
+return (
+ <> 
+  <div className='App' id='editor'>
+    <Editor
+      height="100vh"
+      width="100%" 
+      theme='vs-dark'
+      defaultLanguage='sol'/>
+  </div>       
+ </>
   )
 }
 
