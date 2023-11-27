5d086f4292a1e4b3bc1cadac7d2959e0a970e083
maarten <van_den_bergmaarten@hotmail.com>
2023-11-27 11:23:41 +0100

    added Markdown lessonBox

--- src/LessonTextBox.jsx
+++ src/LessonTextBox.jsx
@@ -1,23 +1,40 @@
 // LessonTextBox.jsx
 
 import React from 'react';
-
+import ReactMarkdown from 'react-markdown';
 
 const LessonTextBox = ({ lessonContent }) => {
+  // Custom components to apply styles to code blocks
+  const components = {
+    code: ({ node, inline, className, children, ...props }) => {
+      // Check if the language is 'solidity'
+      if (!inline && className && className.toLowerCase() === 'language-solidity') {
+        return (
+          <div className="solidity-code">
+            <code>{children}</code>
+          </div>
+        );
+      }
+      // If not 'solidity', use default rendering
+      return <code className={className} {...props}>{children}</code>;
+    },
+  };
+
   return (
     <div className="lesson-textbox">
-      <p>
-      Starting with the absolute basics:
+      <ReactMarkdown components={components}>
+        {`
+Starting with the absolute basics:
 
-Solidity's code is encapsulated in contracts. A contract is the fundamental building block of Ethereum applications — all variables and functions belong to a contract, and this will be the starting point of all your projects.
-
-An empty contract named HelloWorld would look like this:
-
+\`\`\`solidity
 contract HelloWorld {
 
 }
-        {lessonContent}
-      </p>
+\`\`\`
+
+Solidity's code is encapsulated in contracts. A contract is the fundamental building block of Ethereum applications — all variables and functions belong to a contract, and this will be the starting point of all your projects.
+        `}
+      </ReactMarkdown>
     </div>
   );
 };
